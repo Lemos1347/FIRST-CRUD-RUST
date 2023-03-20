7d9dd7d4b974f1f1319b158b9541a372f8eacd70
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-03-20 14:14:58 -0300

    loggers variable changed

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
+    std::env::set_var("RUST_BACKTRACE", "full");
     env_logger::init();
 
     // Creating tables and syncing db
